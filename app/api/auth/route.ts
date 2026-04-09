792fc31f7ea2ae64f00bec764b4f0237dfe1c0ab
Yousef Fouad <youseffouad@icloud.com>
2026-04-09 22:01:07 +0200

    Change gate password

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const PASSWORD = "waaha2025";
+const PASSWORD = "ROzRoZ";
 
 export async function POST(request: Request) {
   const body = await request.json();
